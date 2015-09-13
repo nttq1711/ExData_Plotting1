e4b7d67ab3a6ee92507a71cad110fd6594dfddfe
NTTQ <nttquynh1711@gmail.com>
2015-09-13 09:15:41 -0700

    project

---
+++ plot1.R
@@ -0,0 +1,10 @@
+url <- "household_power_consumption.txt"
+data <- read.table(url, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subsetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
